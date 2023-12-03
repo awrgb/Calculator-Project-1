235f3e34cb0c7aefa3df4dd64e550f07e0abbf20
Abdul Wasea <136252122+awrgb@users.noreply.github.com>
2023-12-03 16:23:44 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
+
 import inquirer from 'inquirer';
-async function simulateShopping() {
-    console.log('Welcome to the Supermarket!');
-    // Scenario 1 - Buying Groceries
-    const groceryChoice = await inquirer.prompt({
-        type: 'list',
-        name: 'groceryType',
-        message: 'What do you want to buy?',
-        choices: ['Fruits', 'Vegetables'],
-    });
-    let items;
-    if (groceryChoice.groceryType === 'Fruits') {
-        items = ['Apple', 'Banana', 'Orange', 'Mango', 'Orange', 'Grapes'];
+async function runCalculator() {
+    const userInput = await inquirer.prompt([
+        {
+            type: 'number',
+            name: 'num1',
+            message: 'Enter the first number:',
+        },
+        {
+            type: 'list',
+            name: 'operation',
+            message: 'Select operation:',
+            choices: ['add', 'subtract', 'multiply', 'divide'],
+        },
+        {
+            type: 'number',
+            name: 'num2',
+            message: 'Enter the second number:',
+        },
+    ]);
+    const { num1, operation, num2 } = userInput;
+    let result;
+    switch (operation) {
+        case 'add':
+            result = num1 + num2;
+            break;
+        case 'subtract':
+            result = num1 - num2;
+            break;
+        case 'multiply':
+            result = num1 * num2;
+            break;
+        case 'divide':
+            if (num2 === 0) {
+                console.log('Error: Cannot divide by zero');
+                return;
+            }
+            result = num1 / num2;
+            break;
+        default:
+            console.log('Invalid operation');
+            return;
     }
-    else {
-        items = ['Carrot', 'Broccoli', 'Tomato', 'Peas', 'Potato', 'Onion'];
-    }
-    console.log(`Available items: ${items.join(', ')}`);
-    const selectedItems = await inquirer.prompt({
-        type: 'checkbox',
-        name: 'selectedItems',
-        message: 'Select items to purchase:',
-        choices: items,
-    });
-    const totalBill = selectedItems.selectedItems.length * 6;
-    console.log(`Total Bill: $${totalBill.toFixed(2)}`);
-    // Scenario 2 - Checking Discounts
-    let discountedTotal = totalBill;
-    if (totalBill > 5) {
-        // Apply a 10% discount if the total bill is above $10
-        discountedTotal = totalBill - totalBill * 0.3;
-        console.log('Discount applied!');
-    }
-    console.log(`Discounted Total: $${discountedTotal.toFixed(2)}`);
-    // Scenario 3 - Checkout Process
-    const paymentMethod = await inquirer.prompt({
-        type: 'list',
-        name: 'paymentMethod',
-        message: 'Choose your payment method:',
-        choices: ['Credit Card', 'Cash', 'Mobile Payment'],
-    });
-    console.log(`You chose to pay with ${paymentMethod.paymentMethod}.`);
-    console.log('Thank you for shopping with us!');
+    console.log(`Result of ${num1} ${operation} ${num2}: ${result}`);
 }
-simulateShopping();
+runCalculator();
