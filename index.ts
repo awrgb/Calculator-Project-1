0f62479367f25ddce551e33b3fc1f424dd22380d
Abdul Wasea <136252122+awrgb@users.noreply.github.com>
2023-12-03 16:22:57 +0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,25 +2,53 @@
 
 import inquirer from 'inquirer';
 
-
-const Details = await inquirer.prompt([
+async function runCalculator() {
+  const userInput = await inquirer.prompt([
     {
-        name: "Name",
-        type: "string",
-        message: "Enter your name: ",
+      type: 'number',
+      name: 'num1',
+      message: 'Enter the first number:',
     },
     {
-        name: "Age",
-        type: "number",
-        message: "Enter your Age: ",
+      type: 'list',
+      name: 'operation',
+      message: 'Select operation:',
+      choices: ['add', 'subtract', 'multiply', 'divide'],
     },
     {
-        name: "Qualification",
-        type: "string, number",
-        message: "Enter your Qualification: "
-    }
-])
+      type: 'number',
+      name: 'num2',
+      message: 'Enter the second number:',
+    },
+  ]);
 
-console.log("Your Name is " + Details.Name)
-console.log("Your Age is " + Details.Age)
-console.log("Your Qualification is " + Details.Qualification)
+  const { num1, operation, num2 } = userInput;
+
+  let result: number;
+
+  switch (operation) {
+    case 'add':
+      result = num1 + num2;
+      break;
+    case 'subtract':
+      result = num1 - num2;
+      break;
+    case 'multiply':
+      result = num1 * num2;
+      break;
+    case 'divide':
+      if (num2 === 0) {
+        console.log('Error: Cannot divide by zero');
+        return;
+      }
+      result = num1 / num2;
+      break;
+    default:
+      console.log('Invalid operation');
+      return;
+  }
+
+  console.log(`Result of ${num1} ${operation} ${num2}: ${result}`);
+}
+
+runCalculator();
